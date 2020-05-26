6f533bd104df332822dc6ef51bde090090ea21c9
sikandar-shah <61984617+sikandar-shah@users.noreply.github.com>
2020-05-26 23:04:10 +0500

    hello worldfor git

--- src/com/java/practise/HelloWorld.java
+++ src/com/java/practise/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		System.out.println("Hello World");
 	}
 
 }
